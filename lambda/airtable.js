a6e799ba691a3e8a2be48378309951ae0dc88b68
Yuri Bondarenko <yuri.bondarenko@scantrust.com>
2019-08-20 03:13:22 +0300

    fix

--- lambda/airtable.js
+++ lambda/airtable.js
@@ -22,15 +22,8 @@ exports.handler = function(event, context, callback) {
     apiKey: process.env.AIRTABLE_API_KEY
   })
 
-<<<<<<< HEAD
-<<<<<<< HEAD
   const base = Airtable.base('appgJj41IS8RduW3N')
-=======
-  const base = Airtable.base('appNtnZ99fkL1cByn')
->>>>>>> parent of 4ec962d... air table api config
-=======
-  const base = Airtable.base('app57ER1ewwOJxjMx')
->>>>>>> parent of 4215d89... origin content
+
   const allRecords = []
   base('entries')
     .select({
