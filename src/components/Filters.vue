6f5a296026e70feb39f26b2d78031f5c569ecfe5
yuriportfolio <37544475+yuriportfolio@users.noreply.github.com>
2019-12-19 16:54:09 +0200

    Update Filters.vue

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container-fluid py-4 text-center">
     <h1 class="text-center">
-      ✨ AEC Startups ✨
+      ✨ FIRSTPERSON.DESIGN ✨
     </h1>
-    <p class="text-secondary">A collection of cool-sounding AEC startups</p>
+    <p class="text-secondary">Augmented reality studio and moder web agency</p>
   </div>
   <!-- <nav class="navbar navbar-light bg-light">
     <span class="navbar-brand mb-0 h1">Navbar</span>
